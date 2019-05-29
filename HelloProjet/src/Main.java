9ea488ef8c871c26865053f0ff969e1506de8d9a
kangweiwei <kangweiwei0712@163.com>
2019-05-29 19:31:16 +0800

    。。。

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("后来的仓库增加的代码!");
+        System.out.println("后来的仓库增加的代码!！");
 
     }
 }
